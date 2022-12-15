6434536013d6778e24e06dd54a7987f40e180fa9
roarinsheep <sminseonong@gmail.com>
2022-12-14 21:42:05 -0800

    takes string

--- morsecode.py
+++ morsecode.py
@@ -19,7 +19,7 @@ def charBreak():
     time.sleep(1/2)
 
 def wordBreak():
-    time.sleep(1)
+    time.sleep(2)
 def A():
     short()
     long()
@@ -53,32 +53,123 @@ def F():
     short()
     long()
     short()
+    charBreak()
+    
+def G():
+    long()
+    long()
+    short()
+    charBreak()
 
 def H():
     for i in range(4):
         short()
     charBreak()
 
+def I():
+    short()
+    short()
+
+def J():
+    short()
+    for i in range(3):
+        long()
+    charBreak()
+
+def K():
+    long()
+    short()
+    long()
+    charBreak()
 
 def L():
     short()
     long()
     short()
     short()
+    charBreak()
+
+def M():
+    long()
+    long()
+    charBreak()
+
+def N():
+    long()
+    short()
+    charBreak()
 
 def O():
     for i in range(3):
         long()
-    
-def W():
+    charBreak()
+
+def P():
     short()
     long()
     long()
-    
+    short()
+    charBreak()
+
+def Q():
+    long()
+    long()
+    short()
+    long()
+    charBreak()
+
 def R():
     short()
     long()
     short()
+    charBreak()
+    
+def S():
+    for i in range(3):
+        short()
+    charBreak()
+
+def T():
+    long()
+    charBreak()
+
+def U():
+    short()
+    short()
+    long()
+    charBreak()
+
+def V():
+    for i in range(3):
+        short()
+    charBreak()
+
+def W():
+    short()
+    long()
+    long()
+    charBreak()
+    
+def X():
+    long()
+    short()
+    short()
+    long()
+    charBreak()
+
+def Y():
+    long()
+    short()
+    long()
+    long()
+    charBreak()
+
+def Z():
+    long()
+    long()
+    short()
+    short()
+    charBreak()
 
     
 charDict = {
@@ -113,8 +204,9 @@ charDict = {
 def StrToMorse(str):
     str = str.lower()
     for c in str:
-        if c in string.ascii_letters:
+        if c.isalpha():
             charDict[c]()
-        elif c in string.whitespace(c):
+        elif c.isspace():
             wordBreak()
 
+StrToMorse("Hello World")
